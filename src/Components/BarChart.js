7dd1fbdc0ad3bdcd687fd3f9f6a83701ba445890
Justin Kerntz <kerntzj@gmail.com>
2019-08-22 11:42:15 -0700

    undo half of stack function for rendering graph

--- src/Components/BarChart.js
+++ src/Components/BarChart.js
@@ -28,15 +28,14 @@ export default class BarChart extends Component {
             .keys(['fahrenheitMinTemp', 'tempDifference'])
             .offset(d3.stackOffsetDiverging)(data)
 
-
         var colors = ['#00D7D2', '#FF4436', '#313c53'];
 
         var g = d3.select('g')
-            .selectAll('g.series')
+            .selectAll('g.addColor')
             .data(stack)
             .enter()
             .append('g')
-            .classed('series', true)
+            .classed('addColor', true)
             .style('fill', (d, i) => {
                 return colors[i];
             });
@@ -53,10 +52,14 @@ export default class BarChart extends Component {
         }))
 
         y.domain([0, d3.max(stack, (d) => {
-            return d3.max(d, (d) => {
-                return d[0] + d[1];
-            });
+            return d3.max(d, (d) => d[1])
         })])
+
+        // y.domain([0, d3.max(stack, (d) => {
+        //     return d3.max(d, (d) => {
+        //         return d[0] + d[1];
+        //     });
+        // })])
 
         //x axis
         svgCanvas.append('g')
@@ -72,18 +75,16 @@ export default class BarChart extends Component {
         //Bar Data to graph
         g.selectAll('rect')
             .data((d) => {
-                console.log(d);
                 // return d;
                 // this fixes the data how it needs to be but breaks, d becomes undefined
                 if (d[0][0] === 0) {
                     return d;
                 } else {
-                    return d.map((diff) => {
-                        console.log(diff)
-                        const temps = diff[0];
-                        return [diff[0],diff.data.tempDifference]
-                        
+                    //change top bar to display the difference
+                    d.forEach((o,i,a) => {
+                        a[i][1] = o.data.tempDifference;
                     })
+                    return d;
                 }
             })
             .enter().append('rect')
