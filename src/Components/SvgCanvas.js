dc5274091e71f70ebed65db8538a416a7201cbea
Justin Kerntz <kerntzj@gmail.com>
2019-08-22 15:33:11 -0700

    add SvgCanvas Component for future refactor

---
+++ src/Components/SvgCanvas.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import * as d3 from 'd3';
+
+export default class SvgCanvas extends Component {
+
+    componentDidMount() {
+        //Plan will be to make an svg component to stay loaded on page and allow component to
+        //rotate in and animate based on lifecycle functions. 
+    }
+
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
