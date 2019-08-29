da0f1995a7b0246c2faeb74ea87bc96f2ef95648
Justin Kerntz <kerntzj@gmail.com>
2019-08-29 15:06:53 -0700

    add smelly conditional to cycle what props pass to RouteManager

--- src/Components/D3Test.js
+++ src/Components/D3Test.js
@@ -7,35 +7,61 @@ class D3Test extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            weeklyWeather: [],
-            routes: [
-                'barChart',
-                'donutGraph',
-                'singleBar',
-                'emojiList'
-            ],
-            currentRoute: 'barChart'
+            data: [],
+            currentRoute: 0,
+            currentKey: 'weeklyWeather',
+            weeklyWeather: {
+                routes: [
+                    'stackedBar',
+                    'donutGraph',
+                    'emojiList'
+                ]
+            },
+            dailyWeather: {
+                routes: [
+                    'singleBar',
+                    'stackedBar'
+                ]
+            },
         }
     }
 
     //routes change every 15 seconds
     changeCurrentRoute = () => {
+        let dataToCycle = Object.keys(this.state).slice(3);//an array of keys avail
+        //i is routes inside of the current j position
         let i = 0;
+        //j is key from state object
+        let j = 0;
+        
+        
+        
+        
+        let routesAvailable = this.state[`${dataToCycle[j]}`].routes;
         setInterval(() => {
-            if (i >= this.state.routes.length - 1) {
+            
+            if (i >= this.state[`${dataToCycle[j]}`].routes.length - 1) {
+                j++
+                   if(j > dataToCycle.length -1)  {
+                       j = 0
+                       routesAvailable = this.state[`${dataToCycle[j]}`].routes;
+                   }
+                routesAvailable = this.state[`${dataToCycle[j]}`].routes;
                 i = 0
-            } else { i++ }
-            this.setState({
-                currentRoute: this.state.routes[i]
-            })
-        }, 15000);
+            } else {
+                i++
+            }
+        }, 3000);
     }
 
     fetchWeatherData = () => {
         let weatherQuery = "https://www.metaweather.com/api/location/2475687/";
         axios.get(weatherQuery).then((response) => {
             const weatherResponse = response.data.consolidated_weather;
-            this.setState({ weeklyWeather: weatherResponse });
+            this.setState({
+                data: weatherResponse,
+            });
+
 
         })
     }
@@ -49,7 +75,7 @@ class D3Test extends Component {
     render() {
         return (
             <div>
-                {this.state.weeklyWeather.length <= 5
+                {this.state.data.length <= 5
                     ? <h1>Loading Graph</h1>
                     : <RouteManager stateHelper={this.state} />
                 }
