b4701aa5744b92ce4cb4942d97668ebe00d24008
Justin <kerntzj@gmail.com>
2019-09-11 16:57:56 -0700

    add function to make objects based on keys, still need the correct values

--- src/Components/D3Test.js
+++ src/Components/D3Test.js
@@ -45,13 +45,30 @@ class D3Test extends Component {
         }
     }
 
+    makeTimberGraphReady = (data) => {
+        let timberData = [];
+        let allKeys = Object.keys(data)
+       console.log(data)
+       for ( let i = 0; i < allKeys.length; i++){ //runs 4 times
+           let dataProps = Object.keys(data[i])
+           console.log(dataProps);
+           let timberDataset = {}
+           for( let j = 0; j < dataProps.length; j++) { //runs 4 times then 2 when j goes to 1
+            timberDataset[dataProps[j]] = 0
+            console.log(timberDataset);
+           }
+       }
+    }
+
+
+
     fetchWeatherData = () => {
         let timbersQuery = 'http://sticky-data.local:8888/projects-dash/?project=timbers';
         axios.get(timbersQuery).then((response) => {
             console.log(response.data)
             let allKeys = Object.keys(response.data);
             //dataset as {{cols:[keys],rows:[values]}} to [{key:[values]},]
-            let data = allKeys.map((c) => {
+            let cleanData = allKeys.map((c) => {
                 let timberData = {}
                 let currentRows = response.data[`${c}`].rows;
                 let currentCols = response.data[`${c}`].cols;
@@ -64,49 +81,35 @@ class D3Test extends Component {
                 })
                 return timberData
             })
-            console.log(data)
+           this.makeTimberGraphReady(cleanData);
         })
-        // let omoQuery = 'http://sticky-data.local:8888/projects-dash/analytics/omo';
-        // axios.get(omoQuery).then((response) => {
-        //     const omo = response.data;
-        //     console.log(omo);
-        //     let omhofQuery = 'http://sticky-data.local:8888/projects-dash/analytics/omhof';
-        //     axios.get(omhofQuery).then((response) => {
-        //         const omhof = response.data;
-        //         console.log(omhof);
-        //         let omhofRawWeeklyData = {}
-        //         omhofRawWeeklyData.weekly = new Object(omhof['kiosks-7day'])
-        //         let omhofRawDailyData = {}
-        //         omhofRawDailyData.daily = new Object(omhof['kiosks-today'])
-        //         const weeklyData = Object.values(omo);
-        //         //weeklyData[x] happens for each route position 
-        //         //since omhof routes each only have 1 route they only need to live inside the 0 position in the prop data to routemanager
-        //         this.setState({
-        //             data: Object.assign(weeklyData[0], omhofRawWeeklyData),
-        //             data: Object.assign(weeklyData[0], omhofRawDailyData),
-        //             data: weeklyData
-        //         });
-        //         this.setState({ loaded: true })
-        //     })
-        // })
+        let omoQuery = 'http://sticky-data.local:8888/projects-dash/analytics/omo';
+        axios.get(omoQuery).then((response) => {
+            const omo = response.data;
+          
+            let omhofQuery = 'http://sticky-data.local:8888/projects-dash/analytics/omhof';
+            axios.get(omhofQuery).then((response) => {
+                const omhof = response.data;
+               
+                let omhofRawWeeklyData = {}
+                omhofRawWeeklyData.weekly = new Object(omhof['kiosks-7day'])
+                let omhofRawDailyData = {}
+                omhofRawDailyData.daily = new Object(omhof['kiosks-today'])
+                const weeklyData = Object.values(omo);
+                //weeklyData[x] happens for each route position 
+                //since omhof routes each only have 1 route they only need to live inside the 0 position in the prop data to routemanager
+                this.setState({
+                    data: Object.assign(weeklyData[0], omhofRawWeeklyData),
+                    data: Object.assign(weeklyData[0], omhofRawDailyData),
+                    data: weeklyData
+                });
+                this.setState({ loaded: true })
+            })
+        })
     }
-
-    // fetchFarWeatherData = () => {
-    //     let weatherQuery = "https://www.metaweather.com/api/location/44418/";
-    //     axios.get(weatherQuery).then((response) => {
-    //         const weatherResponse = response.data.consolidated_weather;
-
-    //         this.setState({
-    //             data: this.state.data.concat(weatherResponse),
-    //         });
-    //         console.log(this.state.data);
-    //     })
-
-    // }
 
     componentDidMount() {
         this.fetchWeatherData()
-        // this.fetchFarWeatherData()
     }
     render() {
         return (
