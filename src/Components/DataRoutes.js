0d68ab6065041996bbc1c63d4ed40c42cdc543fd
Justin Kerntz <kerntzj@gmail.com>
2019-11-14 14:05:58 -0800

    correct colors for barcharts, remove filler pics/add real ones, update list scss

--- src/Components/DataRoutes.js
+++ src/Components/DataRoutes.js
@@ -1,25 +1,25 @@
 export const allRoutes =  {
     omoData: {
       routes: [
-        //   'OHS-CHUTES GAME',
-        //   'OHS-FLECTCHER GAME',
-        //   'OHS-VORTEX GAME',
-        //   'OHS-MARIE GAME',
-                  // 'stackedBarDorian',
-        //   'OHS-CHUTES STORY',
-        //   'OHS-FLECTCHER STORY',
-        //   'OHS-VORTEX STORY',
-        //   'OHS-MARIE STORY',
-          'OHS-MOST READ TODAY',
+          'OHS-CHUTES GAME',
+//           'OHS-FLECTCHER GAME',
+//           'OHS-VORTEX GAME',
+//           'OHS-MARIE GAME',
+//                   //'stackedBarDorian',
+//           'OHS-CHUTES STORY',
+//           'OHS-FLECTCHER STORY',
+//           'OHS-VORTEX STORY',
+//           'OHS-MARIE STORY',
+        //   'OHS-MOST READ TODAY',
         //   'OHS-WEEKLY STORIES READ'
       ]
   },
-  omhof: {
-      routes: [
-        //   'OMHOF TOP AWARDS',
-          'OMHOF AWARD OF THE DAY',
-      ]
-  },
+//   omhof: {
+//       routes: [
+//           'OMHOF TOP AWARDS',
+//           'OMHOF AWARD OF THE DAY',
+//       ]
+//   },
 //   timbersData: {
 //       routes: [
 //         //   'TIMBERS WEEKLY TOP EMOJIS',
