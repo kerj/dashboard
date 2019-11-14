e10b912e3dc359d55997bbcb67eafaf8ea9b12f9
Justin Kerntz <kerntzj@gmail.com>
2019-11-13 17:02:38 -0800

    correct display for most viewed omhof

--- src/Components/DataRoutes.js
+++ src/Components/DataRoutes.js
@@ -1,22 +1,22 @@
 export const allRoutes =  {
     omoData: {
       routes: [
-          'OHS-CHUTES GAME',
-          'OHS-FLECTCHER GAME',
-          'OHS-VORTEX GAME',
-          'OHS-MARIE GAME',
+        //   'OHS-CHUTES GAME',
+        //   'OHS-FLECTCHER GAME',
+        //   'OHS-VORTEX GAME',
+        //   'OHS-MARIE GAME',
                   // 'stackedBarDorian',
-          'OHS-CHUTES STORY',
-          'OHS-FLECTCHER STORY',
-          'OHS-VORTEX STORY',
-          'OHS-MARIE STORY',
+        //   'OHS-CHUTES STORY',
+        //   'OHS-FLECTCHER STORY',
+        //   'OHS-VORTEX STORY',
+        //   'OHS-MARIE STORY',
           'OHS-MOST READ TODAY',
-          'OHS-WEEKLY STORIES READ'
+        //   'OHS-WEEKLY STORIES READ'
       ]
   },
   omhof: {
       routes: [
-          'OMHOF TOP AWARDS',
+        //   'OMHOF TOP AWARDS',
           'OMHOF AWARD OF THE DAY',
       ]
   },
