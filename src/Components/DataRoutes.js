04700c52369a46e2ca6c264ae2a8d2a3d572b937
Justin Kerntz <kerntzj@gmail.com>
2019-11-19 15:01:53 -0800

    add correct colors for donut chart, and fonts

--- src/Components/DataRoutes.js
+++ src/Components/DataRoutes.js
@@ -1,26 +1,26 @@
 export const allRoutes = {
     omoData: {
         routes: [
-            'OHS-CHUTES GAME',
-            'OHS-CHUTES STORY',
-            'OHS-FLECTCHER GAME',
-            'OHS-FLECTCHER STORY',
-            'OHS-VORTEX GAME',
-            'OHS-VORTEX STORY',
-            'OHS-MARIE GAME',
-            'OHS-MARIE STORY',
-            //'stackedBarDorian',
-            'OHS-WEEKLY GAMES VS STORIES',
+            // 'OHS-CHUTES GAME',
+            // 'OHS-CHUTES STORY',
+            // 'OHS-FLECTCHER GAME',
+            // 'OHS-FLECTCHER STORY',
+            // 'OHS-VORTEX GAME',
+            // 'OHS-VORTEX STORY',
+            // 'OHS-MARIE GAME',
+            // 'OHS-MARIE STORY',
+            // 'stackedBarDorian',
+            // 'OHS-WEEKLY GAMES VS STORIES',
             'OHS-WEEKLY STORIES READ',
             'OHS-MOST READ TODAY',
         ]
     },
-    omhof: {
-        routes: [
-            'OMHOF TOP AWARDS',
-            'OMHOF AWARD OF THE DAY',
-        ]
-    },
+    // omhof: {
+    //     routes: [
+    //         'OMHOF TOP AWARDS',
+    //         'OMHOF AWARD OF THE DAY',
+    //     ]
+    // },
     //   timbersData: {
     //       routes: [
     //         //   'TIMBERS WEEKLY TOP EMOJIS',
